de111b1c3d6229f04be1676fdf3e111bbaa7b518
philipp sauerlaender <philipp.sauerlaender@dci-student.org>
2024-11-26 15:10:18 +0100

    change route page

--- src/app/pages/cart/page.js
+++ src/app/pages/cart/page.js
@@ -1,8 +1,28 @@
 "use client";
 
 import styles from "./page.module.css";
+import { TheContext } from "@/components/context-provider/component";
+import { useContext } from "react";
+import { useEffect } from "react";
 
 const Cart = () => {
+    const { localDataBank, dispatch } = useContext(TheContext);
+
+    const fetchCart = async () => {
+        const response = await fetch(`/api/cart/${localDataBank.user._id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    };
+
+    useEffect(() => {
+        localDataBank.user && fetchCart();
+    }, [localDataBank.user]);
+
     return <main>Cart</main>;
 };
 
--- src/models/UsedShoppingCart.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const UsedShoppingCartSchema = new mongoose.Schema({
-    user_id: { type: String, required: true },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UsedItem' }]
-}, { timestamps: true });
-
-export default mongoose.models.UsedShoppingCart || mongoose.model('UsedShoppingCart', UsedShoppingCartSchema);
